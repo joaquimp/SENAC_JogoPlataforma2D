0721fb8211c41c416775117bd1f61403fae3d769
Joaquim Pessôa Filho <joaquimp@gmail.com>
2023-01-06 08:01:52 -0300

    Versão 1.0 - Código de exemplo usado no livro

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,45 +4,45 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    Personagem personagemAtual;
+  Personagem personagemAtual;
 
-    public Personagem personagem1;
+  public Personagem personagem1;
 
-    public Personagem personagem2;
+  public Personagem personagem2;
 
-    void Start()
-    {
-        personagemAtual = personagem1;
-        personagem1.ativo = true;
-        personagem2.ativo = false;
-    }
+  void Start()
+  {
+    personagemAtual = personagem1;
+    personagem1.ativo = true;
+    personagem2.ativo = false;
+  }
 
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.Alpha1)) EscolherPersonagem1();
-        if (Input.GetKey(KeyCode.Alpha2)) EscolherPersonagem2();
-        SeguirPersonagem();
-    }
+  void Update()
+  {
+    if (Input.GetKey(KeyCode.Alpha1)) EscolherPersonagem1();
+    if (Input.GetKey(KeyCode.Alpha2)) EscolherPersonagem2();
+    SeguirPersonagem();
+  }
 
-    void EscolherPersonagem1()
-    {
-        personagemAtual = personagem1;
-        personagem1.ativo = true;
-        personagem2.ativo = false;
-    }
+  void EscolherPersonagem1()
+  {
+    personagemAtual = personagem1;
+    personagem1.ativo = true;
+    personagem2.ativo = false;
+  }
 
-    void EscolherPersonagem2()
-    {
-        personagemAtual = personagem2;
-        personagem1.ativo = false;
-        personagem2.ativo = true;
-    }
+  void EscolherPersonagem2()
+  {
+    personagemAtual = personagem2;
+    personagem1.ativo = false;
+    personagem2.ativo = true;
+  }
 
-    void SeguirPersonagem()
-    {
-        transform.position =
-            new Vector3(personagemAtual.transform.position.x,
-                personagemAtual.transform.position.y + 2.7f,
-                -10);
-    }
+  void SeguirPersonagem()
+  {
+    transform.position =
+        new Vector3(personagemAtual.transform.position.x,
+            personagemAtual.transform.position.y + 2.7f,
+            -10);
+  }
 }
